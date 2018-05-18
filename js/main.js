fb45c099f8a46e516056a09432095d863817533e
konstantin0s <constantintofan85@gmail>
2018-05-18 22:14:52 +0200

    Add marker

--- js/main.js
+++ js/main.js
@@ -3,5 +3,12 @@ function initMap() {
     zoom: 8,
     center: {lat:52.379189, lng:4.899431 }
   }
+
+  //New Map
   var map = new google.maps.Map(document.getElementById('map'), options);
+//Add marker
+var marker = new google.maps.Marker({
+  position: {lat:52.379189, lng:4.899431 },
+  map:map
+});
 }
